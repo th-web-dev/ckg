10f335d612b02910a6c32a60c57d47d4f8e6cc37
tobia <tobias.s.haag@gmail.com>
2025-06-20 22:16:05 +0200

    Added Rust module for generating noise field on client (2)

---
+++ static/wasm/chaoscrypto_noise_bg.wasm.d.ts
@@ -0,0 +1,10 @@
+/* tslint:disable */
+/* eslint-disable */
+export const memory: WebAssembly.Memory;
+export const generate_noise_field: (a: number, b: number, c: number, d: number) => any;
+export const rotate_token: (a: number, b: number, c: number, d: number) => [number, number];
+export const __wbindgen_export_0: WebAssembly.Table;
+export const __wbindgen_malloc: (a: number, b: number) => number;
+export const __wbindgen_realloc: (a: number, b: number, c: number, d: number) => number;
+export const __wbindgen_free: (a: number, b: number, c: number) => void;
+export const __wbindgen_start: () => void;
